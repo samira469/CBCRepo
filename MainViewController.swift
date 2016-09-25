55929950db04c948ced0e9b8595ca6d39a76a93d
Samira <samira.daneshyar@gmail.com>
2016-09-25 09:41:11 -0400

    samira

--- MainViewController.swift
+++ MainViewController.swift
@@ -91,9 +91,6 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        //
-    }
+  
 
 }
